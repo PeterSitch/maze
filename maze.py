82dac7957a45fd30571716235c0446054d8f86f9
PeterSitch <67585984+PeterSitch@users.noreply.github.com>
2021-07-23 10:19:56 +0100

    Add files via upload

--- maze.py
+++ maze.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from PIL import Image
 
 ##https://www.logicmazes.com/bureau/index.htm
 # solution hr,cc,m,cc,im, hr
@@ -7,6 +8,7 @@ import streamlit as st
 st.set_page_config(
     page_title="School maze",
     page_icon=":confused:",
+    layout="wide",
 
  )
 
@@ -31,10 +33,14 @@ def hr(prev):
         
     if prev=='im':
         ##options = ['win']
-        st.markdown(f'## You have permission to go home!  \n You took {st.session_state.steps} moves')
+        col1.markdown(f'## You have permission to go home!  \n You took {st.session_state.steps} moves')
+        col2.image('winner.jpg')
         st.balloons()
     else:
-        st.markdown("She's very sorry but can't take you home until you have permission.  \nShe suggests that you'll need to talk to either:")
+    
+        col2.image('mum.jpeg')
+    
+        col1.markdown("She's very sorry but can't take you home until you have permission.  \nShe suggests that you'll need to talk to either:")
 
         buttons(options)
 
@@ -53,8 +59,10 @@ def m(prev):
         op_str = 'either'
     else:
         op_str = ''
-    st.markdown(f"They shake their head and say you cannot go home yet.  \n"
+        
+    col1.markdown(f"They shake their head and say you cannot go home yet.  \n"
                 f"They suggest that you'll need to talk to {op_str}:")
+    col2.image('mr_johnston.jpeg')
     
     buttons(options)
 
@@ -66,8 +74,9 @@ def im(prev):
         options = ['m','hr']
     elif prev=='m' or prev=='hr':
         options = ['m','cc']
-    st.markdown(f"They see that you have come from {flavour_dict[prev]} but still say you cannot go home yet.  \n"
+    col1.markdown(f"They see that you have come from {flavour_dict[prev]} but still say you cannot go home yet.  \n"
                 f"They suggest that you'll need to talk to either:")
+    col2.image('mrs_randall.jpeg')            
     
     buttons(options)
             
@@ -88,16 +97,18 @@ def cc(prev):
     else:
         op_str = ''
     
-    st.markdown(f"They say they are very sorry but you cannot go home yet.  \n"
+    col1.markdown(f"They say they are very sorry but you cannot go home yet.  \n"
                     f"They suggest that you'll need to talk to {op_str}:")
-
+    
+    col2.image('receptionist.jpeg')
+    
     buttons(options)
 
 def buttons(options):
     for i, option in enumerate(options):
-        st.button(flavour_dict[option], on_click=move,args=(option,)) 
+        col1.button(flavour_dict[option], on_click=move,args=(option,)) 
         if i +1 != len(options):
-            st.write('or')
+            col1.write('or')
 
 
 loc_dict={'hr':hr,'m':m,'cc':cc,'im':im}
@@ -117,11 +128,18 @@ st.title (':confused: School Bureaucracy Maze')
 
 st.info("Welcome to the school bureaucracy maze.  \n You are trying to get permission to go home  \n You'll need to talk to lots of people")
     
+col1,col2 = st.beta_columns(2)
 
-st.markdown(f'## You are talking to {flavour_dict[st.session_state.loc]}')
+col1.markdown(f'## You are talking to {flavour_dict[st.session_state.loc]}')
+
+
+
+
 
 if st.session_state.steps>20:
-    st.markdown('You are very tired')
+    st.markdown(f"You are very tired  \n You've spoken to {st.session_state.steps} people!")
+    st.image('tired_caleb.jpeg',use_column_width ='always')
+
 
     
 loc_dict[st.session_state.loc](st.session_state.prev)
